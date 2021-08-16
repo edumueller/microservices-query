05f97adbbbc15d99c61c1d340c9863ce76266f6e
edumueller <eduardo.nicastro@icloud.com>
2021-08-16 19:02:37 -0300

    feat(posts, events): add routes and initialize

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const PORT = 4002;
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/posts", (req, res) => {});
+
+app.post("/events", (req, res) => {});
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
