52fa786263683b3283380968b5ace1915fbe6349
edumueller <eduardo.nicastro@icloud.com>
2021-08-16 22:26:17 -0300

    feat(event-handler): add event handler func

--- index.js
+++ index.js
@@ -8,24 +8,33 @@ app.use(cors());
 
 const posts = {};
 
+const handleEvent = (type, data) => {
+  switch (type) {
+    case "PostCreated":
+      const { id, title } = data;
+      posts[id] = { id, title, comments: [] };
+      break;
+    case "CommentCreated":
+      const { id, content, postId, status } = data;
+      const post = posts[postId];
+      post.comments.push({ id, content, status });
+      break;
+    case "CommentUpdated":
+      const { id, content, postId, status } = data;
+      const post = posts[postId];
+      const comment = post.comments.find((comment) => comment.id === id);
+      comment.status = status;
+      comment.content = content;
+      break;
+  }
+};
+
 app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
 app.post("/events", ({ body: { type, data } }, res) => {
-  if (type === "PostCreated") {
-    const { id, title } = data;
-    posts[id] = { id, title, comments: [] };
-  }
-  if (type === "CommentCreated") {
-    const { id, content, postId, status } = data;
-    posts[postId].comments = [
-      ...posts[postId].comments,
-      { id, content, status },
-    ];
-  }
-
-  console.log(posts);
+  handleEvent(type, data);
   res.send({});
 });
 
